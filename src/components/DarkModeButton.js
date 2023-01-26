69c3d058e215a367b86c47ae000104c3bd2ad42b
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-26 12:18:50 -0300

    Feat: Add dark mode button

---
+++ src/components/DarkModeButton.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { IconButton } from '@chakra-ui/react'
+import { FaMoon, FaSun } from "react-icons/fa";
+
+function DarkModeButton({colorMode, toggleColorMode}) {
+
+  return (
+    <IconButton 
+    aria-label='dark button'
+    position='absolute' 
+    top='0.8rem' 
+    right='0.8rem' 
+    icon={colorMode === 'light' ? <FaMoon /> : <FaSun />} 
+    variant='ghost'
+    fontSize='3xl'
+    onClick={toggleColorMode}
+    />
+  )
+}
+
+export default DarkModeButton
