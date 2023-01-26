4a3cc355c76cd02fa028340aecca67de4aee7793
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-26 20:24:28 -0300

    Style: Add some space to readability

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,11 +1,20 @@
 import { Select } from '@chakra-ui/select'
 import React from 'react'
 
-function Selector({dataArray, placeholder, addFilterFunc, updateDataFunc, updateFilterFunc, isDisabled}) {
+function Selector({dataArray, placeholder, addFilterFunc, updateDataFunc, updateFilterFunc, isDisabled, isRequired, bg}) {
 
 
   return (
-    <Select placeholder={placeholder} onChange={(e)=>addFilterFunc(e.target.value, updateFilterFunc, updateDataFunc)} value={''} isDisabled={isDisabled}>
+    <Select 
+    placeholder={placeholder} 
+    onChange={(e)=>addFilterFunc(e.target.value, updateFilterFunc, updateDataFunc)} 
+    value={''} 
+    isDisabled={isDisabled} 
+    isRequired={isRequired} 
+    variant='outline' bg={bg}
+    borderWidth={0}
+    boxShadow='lg'
+    >
         {
             dataArray.map( (data) => {
                 return <option key={data} value={data}>{data}</option>
