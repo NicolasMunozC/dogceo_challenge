b2b4e66eec79b9b436de110f3114fc965ef5e4e4
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-29 19:30:32 -0300

    feat(doggysbox): add conditional rendering

--- src/components/DoggysBox/DoggysBox.js
+++ src/components/DoggysBox/DoggysBox.js
@@ -4,33 +4,36 @@ import DoggyCard from './DoggyCard'
 
 function DoggysBox({colorMode, returnToFilters, reqBreeds, reqObj, showData, setShowData, setLoading}) {
 
-  return (
-        <Box w='100%' h='fit-content' mt='2rem' display='flex' flexDir='column' hidden={!showData}>
-          <Button 
-             bg={colorMode === 'light' ? 'blue.600' : 'blue.900'} 
-             colorScheme='blue'
-             textColor={colorMode === 'light' ? 'whiteAlpha.900' : 'whiteAlpa.500'} 
-             mb='1rem'
-             onClick={returnToFilters}
-             boxShadow='lg'
-             children={"Volver"}
-            />
-          { 
-            reqBreeds.map((breed) => {
-              return (
-                <DoggyCard 
-                 key={breed}
-                 colorMode={colorMode}
-                 breed={breed}
-                 reqObj={reqObj}
-                 setShowData={setShowData}
-                 setLoading={setLoading}
-                />
-              )
-            })
-          }
-        </Box>
-  )
+  if(showData){
+    return (
+          <Box w='100%' h='fit-content' mt='2rem' display='flex' flexDir='column' hidden={!showData}>
+            <Button 
+               bg={colorMode === 'light' ? 'blue.600' : 'blue.900'} 
+               colorScheme='blue'
+               textColor={colorMode === 'light' ? 'whiteAlpha.900' : 'whiteAlpa.500'} 
+               mb='1rem'
+               onClick={returnToFilters}
+               boxShadow='lg'
+               children={"Volver"}
+              />
+            { 
+              reqBreeds.map((breed) => {
+                return (
+                  <DoggyCard 
+                   key={breed}
+                   colorMode={colorMode}
+                   breed={breed}
+                   reqObj={reqObj}
+                   setShowData={setShowData}
+                   setLoading={setLoading}
+                  />
+                )
+              })
+            }
+          </Box>
+    )
+  }
+
 }
 
 export default DoggysBox
