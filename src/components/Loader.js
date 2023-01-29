ef38f467c7fef2d77c70aacf9c76c5d8ebbd69bc
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-29 18:47:53 -0300

    Feat(Loader): Add conditional rendering

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,12 +2,14 @@ import { Box, Spinner, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Loader({loading}) {
-  return (
-    <Box w='100%' h='fit-content' mt='2rem' display='flex' flexDir='column' hidden={!loading}>
-    <Spinner mx='auto' mb='1rem' />
-    <Text textAlign='center'>Cargando... </Text>
-  </Box>
-  )
+  if(loading){
+    return (
+      <Box w='100%' h='fit-content' mt='2rem' display='flex' flexDir='column'>
+        <Spinner mx='auto' mb='1rem' />
+        <Text textAlign='center'>Cargando... </Text>
+      </Box>
+    )
+  }
 }
 
 export default Loader
