8bd903183850daae389df77090f0c24dd366065a
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-29 19:29:18 -0300

    feat(selector): add conditional rendering

--- src/components/FilterBox/Selector.js
+++ src/components/FilterBox/Selector.js
@@ -2,35 +2,29 @@ import React from 'react'
 import { Select } from '@chakra-ui/select'
 import { selectedUpdate } from '../../utils'
 
-function Selector( {
-  dataArray,
-  placeholder, 
-  updateDataFunc, 
-  updateFilterFunc, 
-  isDisabled, 
-  isRequired, 
-  bg, 
-}) {
+function Selector( {dataArray, placeholder, updateDataFunc, updateFilterFunc, isDisabled, isRequired, bg}) {
   
+  if(dataArray && dataArray.length > 0){
+    return (
+      <Select 
+      placeholder={placeholder} 
+      onChange={(e)=>selectedUpdate(e.target.value, updateFilterFunc, updateDataFunc)} 
+      value={''} 
+      isDisabled={isDisabled} 
+      isRequired={isRequired} 
+      variant='outline' bg={bg}
+      borderWidth={0}
+      boxShadow='lg'
+      >
+          {
+              dataArray.map( (data) => {
+                  return <option key={data} value={data}>{data}</option>
+              })
+          }
+      </Select>
+    )
+  }
 
-  return (
-    <Select 
-    placeholder={placeholder} 
-    onChange={(e)=>selectedUpdate(e.target.value, updateFilterFunc, updateDataFunc)} 
-    value={''} 
-    isDisabled={isDisabled} 
-    isRequired={isRequired} 
-    variant='outline' bg={bg}
-    borderWidth={0}
-    boxShadow='lg'
-    >
-        {
-            dataArray.map( (data) => {
-                return <option key={data} value={data}>{data}</option>
-            })
-        }
-    </Select>
-  )
 }
 
 export default Selector
