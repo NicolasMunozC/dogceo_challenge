a1043f305c6ca2f3d83d208afd76934893bc2b3d
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-01-30 13:02:44 -0300

    test(selector): fix changes on the comp render

--- src/tests/Selector.test.js
+++ src/tests/Selector.test.js
@@ -3,18 +3,40 @@ import Selector from '../components/FilterBox/Selector'
 
 describe('Selector component', () => {
     it('renders the component', () => {
-        render(<Selector dataArray={['Prueba']}/>)
+        render(
+        <Selector 
+        dataArray={['Prueba']}
+        showData={false}
+        placeholder={'placeholder'}
+        updateDataFunc={()=>{}}
+        updateFilterFunc={()=>{}}
+        isDisabled={false}
+        isRequired={true}
+        bg={'light'}
+        />)
         expect(screen.getByText('Prueba')).toBeInTheDocument()
     })
-
+    
     it('does not renders the component', () => {
-        render(<Selector/>)
-        expect(screen.queryByText('Prueba')).toBeNull()
+        render(
+        <Selector
+        dataArray={['test1', 'test2', 'test']}
+        showData={true}
+        placeholder={'placeholder'}
+        updateDataFunc={()=>{}}
+        updateFilterFunc={()=>{}}
+        isDisabled={false}
+        isRequired={true}
+        bg={'light'}
+        />
+        )
+        expect(screen.queryByText('test2')).toBeNull()
     })
 
     it('dataArray length = 1', () => {
         render(
         <Selector
+        showData={false}
         dataArray={['test']}
         placeholder={'placeholder'}
         updateDataFunc={()=>{}}
@@ -30,6 +52,7 @@ describe('Selector component', () => {
     it('dataArray length > 1', () => {
         render(
         <Selector
+        showData={false}
         dataArray={['test1', 'test2', 'test']}
         placeholder={'placeholder'}
         updateDataFunc={()=>{}}
